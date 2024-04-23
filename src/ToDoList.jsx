1c10bac103eab799d4ee9bd00f4c172c4287829f
Kriidfel <gillou8@hotmail.com>
2024-04-23 22:24:08 +0200

    Adding some features

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect} from 'react';
 import ToDoItem from './components/ToDoItem';
 import ToDoForm from "./components/ToDoForm.jsx";
+import DeleteAllButton from "./components/DeleteAllButton.jsx";
+
 // eslint-disable-next-line react/prop-types
 
 const LSKEY = "MyTodoApp";
@@ -12,27 +14,50 @@ function ToDoList() {
 
     const [todos, setTodos] = useState(storedTodos  ? JSON.parse(storedTodos) : [])
     const [editingId, setEditingId] = useState(null);
+    const [allChecked, setAllChecked] = useState(false);
 
     const handleCheck = index => {
-        setTodos(todos.map(todo => {
-            if (todo.id === index) {
-                return {
-                    ...todo,
-                    checked: !todo.checked
-                };
-            }
-            return todo;
-        }));
+        setTodos(prevTodos => {
+            const newTodos = prevTodos.map(todo => {
+                if (todo.id === index) {
+                    return {
+                        ...todo,
+                        checked: !todo.checked
+                    };
+                }
+                return todo;
+            });
+
+            // Check if all todos are checked
+            const allChecked = newTodos.every(todo => todo.checked);
+            setAllChecked(allChecked);
+
+            return newTodos;
+        });
+    };
+
+    const handleCheckAll = () => {
+        if (todos.length === 0) return;
+        setTodos(todos.map(todo => ({...todo, checked: !allChecked})));
+        setAllChecked(!allChecked);
     };
 
     const handleDelete = index => {
+    if (index === undefined) {
+        setTodos(todos.filter(todo => !todo.checked));
+        if (todos.every(todo => todo.checked)) {
+            setAllChecked(false);
+        }
+    } else {
         const todoIndex = todos.findIndex(todo => todo.id === index);
         const newTodos = [...todos];
         newTodos.splice(todoIndex, 1);
         setTodos(newTodos);
-        //setTodos(todos.filter((todo) => todoIndex !== index));
-
-    };
+        if (newTodos.length === 0) {
+            setAllChecked(false);
+        }
+    }
+};
 
     const handleEdit = (id) => {
         setEditingId(id);
@@ -56,21 +81,47 @@ function ToDoList() {
         window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
     }, [todos]);
 
+    function countUnCHeck() {
+        const count=  todos.filter(todo => !todo.checked).length;
+        return count.toString().padStart(2, '0');
+    }
+
     return (
         <>
             <ToDoForm handleAdd = {handleAdd} />
             <ul className={"allTodo"}>
-                <h2>ToDos</h2>
+
+                <div className={"header"}>
+                    <div className={"header-all"}>
+                        <div className={"header-check"}>
+                            <input type="checkbox" className={"checkAll"} id="checkAll" name="checkAll"  checked={allChecked}
+                                   onChange={handleCheckAll}/>
+                            <label>All</label>
+                        </div>
+                        <div className={"header-delete"}>
+                            <DeleteAllButton onDelete={() => handleDelete()} todo={todos.map((todo) => todo)} />
+                        </div>
+                    </div>
+                    <div className={"header-title"}>
+                        <h2>ToDos</h2>
+                    </div>
+                    <div className={"header-counter"}>
+                        <p className={"count"}>{countUnCHeck()} / {todos.length.toString().padStart(2, '0')}</p>
+                    </div>
+                </div>
+
                 {todos.map((todo) => (
-                    <ToDoItem
-                        key={todo.id}
-                        todo={todo}
-                        onCheck={() => handleCheck(todo.id)}
-                        onDelete={() => handleDelete(todo.id)}
-                        onEdit={()=> handleEdit(todo.id)}
-                        onUpdate={handleUpdate}
-                        editing={todo.id === editingId}
-                    />
+                    <>
+                        <ToDoItem
+                            key={todo.id}
+                            todo={todo}
+                            onCheck={() => handleCheck(todo.id)}
+                            onDelete={() => handleDelete(todo.id)}
+                            onEdit={()=> handleEdit(todo.id)}
+                            onUpdate={handleUpdate}
+                            editing={todo.id === editingId}
+                        />
+                    </>
                 ))}
             </ul>
         </>
--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,4 +1,5 @@
 import {useEffect, useRef, useState} from "react";
+import DeleteButton from './DeleteButton';
 
 // eslint-disable-next-line react/prop-types
 export default function ToDoItem({ todo, onCheck, onDelete, onEdit, onUpdate, editing }) {
@@ -22,7 +23,7 @@ export default function ToDoItem({ todo, onCheck, onDelete, onEdit, onUpdate, ed
     return (
         <li className={"todo"}>
             <input className={"check"} type="checkbox" id={todo.id} name={todo.text} checked={todo.checked} onChange={onCheck}/>
-            {editing ? (
+            {(editing) ? (
                 <>
                     <form className={"editForm"} onSubmit={handleSubmit}>
                         <input className={"editTodo"}
@@ -32,14 +33,18 @@ export default function ToDoItem({ todo, onCheck, onDelete, onEdit, onUpdate, ed
                                onBlur={handleSubmit}
                                ref={inputRef}
                         />
-                        <button type="submit">Update</button>
+                        <button className={"validate"} type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                                <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
+                            </svg>
+                        </button>
                     </form>
 
 
                 </>
             ) : (
                 <>
-                    <p className={todo.checked ? "text checked" : "text"} onClick={() => onEdit(todo.id)}>{todo.text}</p>
+                    <p className={todo.checked ? "text checked" : "text"} onClick={() => !todo.checked && onEdit(todo.id)}>{todo.text}</p>
 
                     <button className={"edit"} onClick={onEdit} disabled={todo.checked}>
                         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
@@ -47,12 +52,7 @@ export default function ToDoItem({ todo, onCheck, onDelete, onEdit, onUpdate, ed
                                 d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/>
                         </svg>
                     </button>
-                    <button className={"delete"} onClick={onDelete} disabled={!todo.checked}>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                            <path
-                                d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
-                        </svg>
-                    </button>
+                    <DeleteButton onDelete={onDelete} todo={todo} />
                 </>
             )}
 
