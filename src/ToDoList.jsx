6d72017da77a69e126a305e0c9c9566dfa762ded
Kriidfel <kriidfel@Kriidfel>
2024-04-23 19:37:31 +0200

    Change in edit and adding local storage

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import ToDoItem from './components/ToDoItem';
 import ToDoForm from "./components/ToDoForm.jsx";
 // eslint-disable-next-line react/prop-types
 
+const LSKEY = "MyTodoApp";
+
 function ToDoList() {
-    const initialTodos = [ {
-        id: 1,
-        text: 'Learn React',
-        checked: false
-    }];
-    const [todos, setTodos] = useState(initialTodos);
+
+
+    const storedTodos = window.localStorage.getItem(LSKEY + ".todos");
+
+    const [todos, setTodos] = useState(storedTodos  ? JSON.parse(storedTodos) : [])
+    const [editingId, setEditingId] = useState(null);
 
     const handleCheck = index => {
         setTodos(todos.map(todo => {
@@ -29,19 +31,19 @@ function ToDoList() {
         newTodos.splice(todoIndex, 1);
         setTodos(newTodos);
         //setTodos(todos.filter((todo) => todoIndex !== index));
+
     };
 
-    const handleEdit = index => {
-        const todoIndex = todos.findIndex(todo => todo.id === index);
-        const newTodos = [...todos];
-        const newText = prompt("Edit Todo", newTodos[todoIndex].text); // /!\ edit check id list not id todo
-        if (newText !== null && newText.trim() !== ''){
-            newTodos[todoIndex].text = newText;
-            setTodos(newTodos);
-        }
+    const handleEdit = (id) => {
+        setEditingId(id);
     };
 
-    const handleAdd = (newTodo) => {
+    const handleUpdate = (id, newText) => {
+        setTodos(todos.map(todo => todo.id === id ? {...todo, text: newText} : todo));
+        setEditingId(null);
+    };
+
+    const handleAdd = newTodo => {
         setTodos([...todos, {
             id: new
             Date().getTime(),
@@ -49,6 +51,10 @@ function ToDoList() {
             checked: false
         }]);
     }
+
+    useEffect(() => {
+        window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
+    }, [todos]);
 
     return (
         <>
@@ -62,6 +68,8 @@ function ToDoList() {
                         onCheck={() => handleCheck(todo.id)}
                         onDelete={() => handleDelete(todo.id)}
                         onEdit={()=> handleEdit(todo.id)}
+                        onUpdate={handleUpdate}
+                        editing={todo.id === editingId}
                     />
                 ))}
             </ul>
